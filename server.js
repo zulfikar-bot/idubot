5437472002886094cf729dfe2384ee779cd35c64
Glitch (glitch-blank-node) <none>
2022-07-13 08:35:04 +0000

    🐸📫 Checkpoint ./server.js:40019640/906

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const fs = require('fs')
 const port = process.env.PORT
 
 const baileys = require('@adiwajshing/baileys')
@@ -15,6 +16,22 @@ async function start() {
   sock.ev.on('creds.update', saveCreds)
   sock.ev.on('connection.update', update => {
     if (update.qr) console.log(`RECEIVED QR\n${update.qr}`)
+    if (update.connection === 'close') {
+//       if (update.lastDisconnect.error.output.statusCode === 401) {
+        
+//       }
+      const { connection, lastDisconnect } = update
+        if(connection === 'close') {
+            const shouldReconnect = lastDisconnect.error.output.statusCode === 401
+            console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
+            // reconnect if not logged out
+            if(shouldReconnect) {
+                start()
+            }
+        } else if(connection === 'open') {
+            console.log('opened connection')
+        }
+    }
   })
 }
 
