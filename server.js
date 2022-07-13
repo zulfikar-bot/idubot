ead3e38be0c1fb3c0a2d6b0352c3f2e784df3c37
Glitch (glitch-blank-node) <none>
2022-07-13 08:23:09 +0000

    🌅👼 Checkpoint ./server.js:40019640/480

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ http.createServer((_,res) => {
 }).listen(port)
 console.log('Server runs at port',port)
 
+async function start() {
+  const {state, saveCreds} = await useMultiFileAuthState('./.data/wa_creds/')
+  const sock = baileys.default({auth:state})
+  sock.ev.on('creds.update', saveCreds)
+  sock.ev.on('connection.update', update => {
+    if (update.qr) console.log(`RECEIVED QR\n${update.qr}`)
+  })
+}
 
-
-const sock = baileys.default()
+start()
