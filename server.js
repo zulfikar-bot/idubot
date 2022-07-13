63fae21b80897c3da71c54821ecc1a6bd7dc71f5
Glitch (glitch-blank-node) <none>
2022-07-13 08:11:06 +0000

    🏡🔮 Checkpoint ./server.js:40019640/283

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const http = require('http')
 const port = process.env.PORT
 
 const baileys = require('@adiwajshing/baileys')
+const {useMultiFileAuthState} = baileys
 
 http.createServer((_,res) => {
   res.end('Server is running')
 }).listen(port)
 console.log('Server runs at port',port)
 
-console.log(typeof baile)
+
+
+const sock = baileys.default()
