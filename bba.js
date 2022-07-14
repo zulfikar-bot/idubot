c793d83c3d97a98518a334e171a33bb420a10e51
Glitch (glitch-blank-node) <none>
2022-07-14 05:52:15 +0000

    🍂💳 Checkpoint ./bba.js:40019640/420

--- bba.js
+++ bba.js
@@ -1,9 +1,11 @@
 const {download, request} = require('tools')
+const {randomInt} = require('crypto')
 
 const token = process.env.GITHUB_TOKEN
 const repo = 'aidulcandra/materi-bahasa-asing'
 
 const materialList = {}
+const fileCache = {}
 
 async function getMaterialList(code) {
   if (!materialList[code]) {
@@ -12,12 +14,14 @@ async function getMaterialList(code) {
 }
 
 async function getFile(path) {
-  return (await request('GET', `https://github.com/${repo}/raw/main/${path}`)).response
+  if (fileCache[path]) {return fileCache[path]}
+  fileCache[path] = (await request('GET', `https://github.com/${repo}/raw/main/${path}`)).response
+  return 
 }
 
 module.exports = {
   getRandomMaterial: async(code)=>{
     const list = await getMaterialList(code)
-    
+    return (await getFile(`${code}/files/${list[randomInt(list.length)].link}`))
   }
 }
